a0b8369186773c857c8d1b3301a35fdbdb0c8910
Andrea <anguerra@pdx.edu>
2019-11-25 02:15:51 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -40,16 +40,13 @@ $(function () {
 	slideCount = 5;
 	function timeout() {
 		setTimeout(function() {
-			// Move $("#quote" + i) off to the left
 			$("#quote" + i).animate({
 				right: "120%"
 			}, 2000);
-			// Change selected quote
 			i++;
 			if(i > (slideCount - 1)) {
 				i = 0;
 			}
-			// Move $("#quote" + i) to right side then back to middle
 			$("#quote" + i).css("right", "-60%");
 			$("#quote" + i).animate({
 				right: "20%"
