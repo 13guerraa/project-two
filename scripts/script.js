aa429ad60fb1b973128d69dcdfb2f2c5027ee5aa
Andrea <anguerra@pdx.edu>
2019-12-09 18:56:43 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 // your script file for adding your own jquery
+
+
 $(function () {
     // Your Code from here on down. Don't delete that line above!
 
-    'use strict'
+
 
 
     function toggleIcon() {
@@ -16,39 +18,103 @@ $(function () {
         $('.menu-mobile').on('click', 'a', function () {
             $('.icon').trigger('click');
         })
+
     }
 
+    var i = 0;
+    slideCount = 3;
 
+    function timeout() {
+        setTimeout(function () {
+            $("#quote" + i).animate({
+                right: "120%"
+            }, 1000);
+            i++;
+            if (i > (slideCount - 1)) {
+                i = 0;
+            }
+            $("#quote" + i).css("right", "-60%");
+            $("#quote" + i).animate({
+                right: "20%"
+            }, 1000);
+            timeout();
+        }, 3000);
+    };
+    timeout();
 
-   
 
     //when the page loads call toggleIcon;
     $(toggleIcon);
     $(closeMobileMenu);
     $(desktopMenu);
 
-
-
-    var i = 0;
-	slideCount = 5;
-	function timeout() {
-		setTimeout(function() {
-			$("#quote" + i).animate({
-				right: "120%"
-			}, 2000);
-			i++;
-			if(i > (slideCount - 1)) {
-				i = 0;
-			}
-			$("#quote" + i).css("right", "-60%");
-			$("#quote" + i).animate({
-				right: "20%"
-			}, 2000);
-			timeout();
-		}, 7000);
-	};
-	timeout();
-
-
-    // End of Your Code . Don't delete that line below!!
 });
+
+$(document).ready(function () {
+    "use strict";
+
+    //define menu filtering visibility function
+    function menuFilter(categoryClass, button) {
+        $(".menuListContainer").removeClass("allMenuColor");
+        $(".button").removeClass("activeButton");
+        button.addClass("activeButton");
+        $(".menuList").hide("fast");
+        $(categoryClass).show("fast");
+    }
+
+    //button click event handlers
+    $("#btnAll").click(function () {
+        $(".button").removeClass("activeButton");
+        $(".menuListContainer").addClass("allMenuColor");
+        $(".menuList").show("fast");
+    });
+
+    $("#btnHotline").click(function () {
+        menuFilter($(".hotline"), $("#btnHotline"));
+    });
+
+    $("#btnBeef").click(function () {
+        menuFilter($(".beef"), $("#btnBeef"));
+    });
+
+    $("#btnSpecialty").click(function () {
+        menuFilter($(".specialty"), $("#btnSpecialty"));
+    });
+
+    $("#btnCrispy").click(function () {
+        menuFilter($(".crispy"), $("#btnCrispy"));
+    });
+
+    $("#btnSandwiches").click(function () {
+        menuFilter($(".sandwiches"), $("#btnSandwiches"));
+    });
+
+    $("#btnBurgers").click(function () {
+        menuFilter($(".burgers"), $("#btnBurgers"));
+    });
+});
+
+$(document).ready(function () {
+    $("#submit").click(function () {
+        alert("You're form has been submitted. Thank you!");
+    });
+
+});
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
